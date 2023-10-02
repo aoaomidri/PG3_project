6d2590c728962660f33a8e0bddfcbf04a981247a
aoaomidri <aoaomidori2723@gmail.com>
2023-10-02 12:17:18 +0900

    osos

--- main.cpp
+++ main.cpp
@@ -1,30 +1,20 @@
 ﻿#include<iostream>
+int SalaryComparison(int n,int count) {
+	int BaseSalary = 1072;
+	int returnSalary = 0;
+	if (BaseSalary*count<)
+	{
 
-template <typename Type>
-Type Min(Type a, Type b) {
-	if (a > b) {
-		return b;
 	}
-	else if (b > a) {
-		return a;
-	}
-}
-
-template<>
-char Min<char>(char a, char b) {
-	std::wcout << "数字以外は代入できません" << std::endl;
-	return '\n';
+	returnSalary = returnSalary + n * 2 - 50;
+	return n * 2 - 50;
 }
 
 int main(void) {
-	int a = 8, b = 4;
-	float fa = 9.4f, fb = 9.39f;
-	double da = 973, db = 918;
-	char ca = 'g', cb = 'n';
+	int firstSalary = 100;
 
-	std::cout << Min(a, b) << std::endl;
-	std::cout << Min(fa, fb) << std::endl;
-	std::cout << Min(da, db) << std::endl;
-	std::cout << Min(ca, cb) << std::endl;
+	int count = 1;
+
+
 	return 0;
 }
