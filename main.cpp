ed375757cc21efda6a5cb9883c2a721ea6873283
aoaomidri <aoaomidori2723@gmail.com>
2023-10-02 12:56:40 +0900

    01_02完了

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 ﻿#include<iostream>
-int SalaryComparison(int n,int count) {
+int SalaryComparison(int n,int count,int num) {
+	int BaseSalary = 1072;
+	int BaseNum = 0;
+
+	BaseNum = BaseSalary * count;
+
+	if (BaseNum < num){
+		return count;
+	}
+	else {
+		num += n * 2 - 50;
+
+		return SalaryComparison(n * 2 - 50, count + 1, num);
+	}
 	
-	
-	return n * 2 - 50;
 }
 
 int main(void) {
 	int firstSalary = 100;
-
 	int count = 1;
+	int num = 100;
+	int result = 0;
 
+	result = SalaryComparison(firstSalary, count, num);
+
+	printf("%d", result);
 
 	return 0;
 }
