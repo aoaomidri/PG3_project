900e5843f3613ff6435cf522ee573694ec8d5df0
aoaomidri <aoaomidori2723@gmail.com>
2023-12-18 10:39:46 +0900

    hoge

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
 ﻿#include<iostream>
+#include<vector>
 
-template <typename Type>
-Type Min(Type a, Type b) {
-	if (a > b) {
-		return b;
-	}
-	else if (b > a) {
-		return a;
-	}
-}
-
-template<>
-char Min<char>(char a, char b) {
-	std::wcout << "数字以外は代入できません" << std::endl;
-	return '\n';
-}
 
 int main(void) {
-	int a = 8, b = 4;
-	float fa = 9.4f, fb = 9.39f;
-	double da = 973, db = 918;
-	char ca = 'g', cb = 'n';
+	int x = 0;
+	int y = 0;
+	
+	
+	while (true) {
+		
+		std::cin >> x >> y;
+		if (x == 0 && y == 0){
+			break;
+		}
 
-	std::wcout << Min(a, b) << std::endl;
-	std::wcout << Min(fa, fb) << std::endl;
-	std::wcout << Min(da, db) << std::endl;
-	Min(ca, cb);
+		if (x > y) {
+			std::cout << y << " " << x << std::endl;
+		}
+		else {
+			std::cout << x << " " << y << std::endl;
+		}
+	}
+
 	return 0;
 }
