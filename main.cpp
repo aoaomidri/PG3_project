4b388613a166c0d4eaaf7dfcde6859f9dea84ab6
aoaomidri <aoaomidori2723@gmail.com>
2024-02-05 12:28:01 +0900

    06_02再投稿

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
-﻿#include"Enemy.h"
+﻿#include <iostream>
+#include<thread>
+void DrawThread(int num) {
+    std::cout << "thread " << num << std::endl;
+}
 
-int main(void) {
-	Enemy enemy{};
+void DrawThreadAddOne(int num) {
+    num += 1;
+    DrawThread(num);
+}
 
-	enemy.Initialize();
+void DrawThreadAddTwo(int num) {
+    num += 2;
+    DrawThread(num);
+}
 
-	enemy.Update();
+int main() {
+    int num = 1;
 
+    std::thread th1(DrawThread, num);
+    th1.join();
+    std::thread th2(DrawThreadAddOne, num);
+    th2.join();
+    std::thread th3(DrawThreadAddTwo, num);
+    th3.join();
 
-	return 0;
+    return 0;
 }
