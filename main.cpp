148eb01d263aa2eab3e1cc3284f11bc6d3be0f06
aoaomidri <aoaomidori2723@gmail.com>
2024-03-01 16:13:18 +0900

    aizu7完了

--- main.cpp
+++ main.cpp
@@ -1,22 +1,50 @@
-﻿#include<iostream>
-#define _USE_MATH_DEFINES
-#include<math.h>
-#include<string>
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::string filePath = "resources/uvChecker.png";
+    int n, m, l;
+    cin >> n >> m >> l;
 
-    size_t slashPos = filePath.find_last_of('/');
-    size_t dotPos = filePath.find_last_of('.');
+    // 行列AとBの定義
+    vector<vector<long>> A(n, vector<long>(m));
+    vector<vector<long>> B(m, vector<long>(l));
 
-    if (slashPos != std::string::npos && dotPos != std::string::npos && dotPos > slashPos) {
-        std::string result = filePath.substr(slashPos + 1, dotPos - slashPos - 1);
-        std::cout << result << std::endl;
+    // 行列Aの入力
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> A[i][j];
+        }
     }
-    else {
-        std::cout << "Substring not found." << std::endl;
+
+    // 行列Bの入力
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < l; ++j) {
+            cin >> B[i][j];
+        }
+    }
+
+    // 行列C（結果を保存する行列）の初期化
+    vector<vector<int>> C(n, vector<int>(l, 0));
+
+    // 行列の積計算
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            for (int k = 0; k < m; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    // 結果の出力
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            cout << C[i][j];
+            if (j < l - 1) cout << " ";  // 行の最後以外では空白を出力
+        }
+        cout << endl;
     }
 
     return 0;
-
 }
